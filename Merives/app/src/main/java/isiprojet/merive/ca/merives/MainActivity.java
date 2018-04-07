70f724145a39cd1bd093b43479880e335c40314c
fred <fredeyerkagnou@gmail.com>
2018-04-07 18:52:09 -0400

    architecture du projet merives.

---
+++ Merives/app/src/main/java/isiprojet/merive/ca/merives/MainActivity.java
@@ -0,0 +1,13 @@
+package isiprojet.merive.ca.merives;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
